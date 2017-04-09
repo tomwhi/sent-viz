41298c53c91f2213332531807da8ce494b0f3db2
Thomas Whitington <thomaswhitington@gmail.com>
2017-04-09 20:38:01 +0200

    Cleaned up R script + changed to using ggplot, and giving up connecting sentences within a paragraph for the time-being.

--- viz/vis_vectors.R
+++ viz/vis_vectors.R
@@ -1,14 +1,10 @@
 library(ggplot2)
-x <- read.table("tSNE_results_2components.txt", sep="\t", header=TRUE)
-filt<-x[x$topic == "Charlie Parker",]
+args <- commandArgs(TRUE)
 
+input_filename <- args[1]
+output_filename <- args[2]
 
-png("1vs2_tSNE_3components.png", width=1000, height=1000)
-plot(0, xlim=c(-20,20), ylim=c(-20,20), cex=0.01)
-for (paragraph in unique(filt$paragraph_idx)) {
-  curr_par <- filt[filt$paragraph_idx == paragraph,]
-  text(curr_par$comp1[1], curr_par$comp2[1], curr_par$paragraph_idx, col=curr_par$paragraph_idx)
-  lines(curr_par$comp1, curr_par$comp2, cex=1, col=curr_par$paragraph_idx)
-  points(curr_par$comp1, curr_par$comp2, cex=0.5, pch=16, col=curr_par$paragraph_idx)
-}
-dev.off()
+sentence_data <- read.table(input_filename, sep="\t", header=TRUE)
+
+ggplot(sentence_data, aes(comp1, comp2)) + geom_point(aes(colour=topic, alpha=sentence_idx))
+ggsave(output_filename)
