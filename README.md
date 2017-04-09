5bfc259501f9863b2734dc35e8c91b9dd138c36f
Thomas Whitington <thomaswhitington@gmail.com>
2017-04-09 20:59:28 +0200

    Added e.g. to README, added separate plot generation for sklearn components.

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ conda install -y scipy
 conda install -y pandas
 pip install git+https://github.com/tomwhi/skip-thoughts.git
 ```
+
+# Running
+Example:
+
+```
+python /path/to/process_topics.py --topics-str="Tokyo,Kyoto,koala,platypus,Jackie Mclean,Kenny Garrett" --output="SentenceData.txt"
+Rscript /path/to/vis_vectors.R SentenceData.txt SentenceViz
+```
--- viz/vis_vectors.R
+++ viz/vis_vectors.R
@@ -2,9 +2,13 @@ library(ggplot2)
 args <- commandArgs(TRUE)
 
 input_filename <- args[1]
-output_filename <- args[2]
+output_prefix <- args[2]
 
 sentence_data <- read.table(input_filename, sep="\t", header=TRUE)
 
-ggplot(sentence_data, aes(comp1, comp2)) + geom_point(aes(colour=topic, alpha=sentence_idx))
-ggsave(output_filename)
+ggplot(sentence_data, aes(comp1, comp2)) + geom_point(aes(colour=topic))#, alpha=sentence_idx
+ggsave(paste(output_prefix, "_bhtsne.png"))
+
+ggplot(sentence_data, aes(comp1_sklearn, comp2_sklearn)) + geom_point(aes(colour=topic))#, alpha=sentence_idx
+ggsave(paste(output_prefix, "_sklearn.png"))
+              
